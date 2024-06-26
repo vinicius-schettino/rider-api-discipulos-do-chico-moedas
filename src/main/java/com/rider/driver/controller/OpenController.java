69a95f84dbeb9bb738df3a42fa37648cc7f251d3
Bernardo <bernardoalvesbrum_100@hotmail.com>
2024-06-26 09:58:41 -0300

    feat: new authorization enviroment

---
+++ src/main/java/com/rider/driver/controller/OpenController.java
@@ -0,0 +1,17 @@
+package com.rider.driver.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/open")
+public class OpenController {
+
+    @GetMapping
+    public ResponseEntity<Void> security() {
+        return ResponseEntity.ok().build();
+    }
+
+}
